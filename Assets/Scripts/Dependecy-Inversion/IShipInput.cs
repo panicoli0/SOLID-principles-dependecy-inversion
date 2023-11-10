f1646e02335e8530756d48624da3156dd2ae4365
Pablo Ariel Nicoli <pablonicoli21@gmail.com>
2023-11-10 13:40:43 +0400

    Created IShipInput interface

---
+++ Assets/Scripts/Dependecy-Inversion/IShipInput.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SOLID.DependencyInversion
+{
+    internal interface IShipInput
+    {
+        void ReadInput();
+        float Thrust { get; }
+        float Rotation { get; }
+        
+    }
+}
