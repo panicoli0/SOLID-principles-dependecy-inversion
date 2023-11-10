5bece63500b965c10a354d5a677d53678f7266d8
Pablo Ariel Nicoli <pablonicoli21@gmail.com>
2023-11-10 13:40:14 +0400

    Created ShipMotor constructor with Tick behavior

---
+++ Assets/Scripts/Dependecy-Inversion/ShipMotor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SOLID.DependencyInversion
+{
+    internal class ShipMotor
+    {
+        private readonly IShipInput shipInput;
+        private readonly Transform transformToMove;
+        private readonly ShipSettings shipSettings;
+
+        public ShipMotor(IShipInput shipInput, Transform transformToMove, ShipSettings shipSettings)
+        {
+            this.shipInput = shipInput;
+            this.transformToMove = transformToMove;
+            this.shipSettings = shipSettings;
+        }
+
+        internal void Tick()
+        {
+            transformToMove.Rotate(shipInput.Rotation * shipSettings.TurnSpeed * Time.deltaTime * Vector3.up);
+            transformToMove.position += shipInput.Thrust * shipSettings.TurnSpeed * Time.deltaTime * transformToMove.forward;
+        }
+    }
+}
