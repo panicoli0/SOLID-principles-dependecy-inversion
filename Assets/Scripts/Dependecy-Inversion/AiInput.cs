e2facbe29e447e93513bbe19a9640a7faea07e43
Pablo Ariel Nicoli <pablonicoli21@gmail.com>
2023-11-10 13:41:59 +0400

    Created ControllerInput and AiInput whichs enerith from IShipInput interface

---
+++ Assets/Scripts/Dependecy-Inversion/AiInput.cs
@@ -0,0 +1,17 @@
+﻿using Random = UnityEngine.Random;
+
+namespace SOLID.DependencyInversion
+{
+    internal class AiInput : IShipInput
+    {
+        public float Thrust { get; private set; }
+
+        public float Rotation { get; private set; }
+
+        public void ReadInput()
+        {
+            Rotation = Random.Range(-1f, 1f);
+            Thrust = Random.Range(0f, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/Dependecy-Inversion/ControllerInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace SOLID.DependencyInversion
+{
+    internal class ControllerInput : IShipInput
+    {
+        public void ReadInput()
+        {
+            Rotation = Input.GetAxis("Horizontal");
+            Thrust = Input.GetAxis("Vertical");
+        }
+
+        public float Thrust { get; private set; }
+        public float Rotation { get; private set; }
+    }
+}
